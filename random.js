46fdea6a301f79fb4b13f7941b9e83f829731bc9
Sandy Gomez <gomezsandy01.14sg@gmail.com>
2017-03-28 14:23:51 -0700

    asdf

---
+++ random.js
@@ -0,0 +1,186 @@
+var namespace = "http://www.w3.org/2000/svg"
+// Fill in this function so that it draws something using SVG shapes!
+// You need to use at least 3 different types of shape.
+// Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
+function createFirstScene() {   
+    //pigNose
+    makeEllipse(50,30,50,30,"#FF2052",0.50)   
+    var x = Math.random()
+     makeCircle(30,30,13,"#3B444B",1)  
+    if(x , 0.55) {
+       makeCircle(70,30,13,"#3B444B",1)
+    } else {
+      makeCircle(30,30,13,"#3B444B",1)   
+    }
+    }
+       
+
+
+
+ 
+// Fill in this function so that it draws something using SVG shapes!/ You need to use at least 3 different types of shape.
+// Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
+function createSecondScene() {
+    //face
+   makeCircle(90,100,50,"#CC474B",1)
+   makeCircle(70,80,10,"#C39953",1)
+   makeCircle(100,80,10,"#C39953",1)
+   makeCircle(70,80,5,"#96CBA2",1)
+   makeCircle(100,80,5,"#96CBA2",1)
+}
+
+
+
+// Fill in this function so that it draws something using SVG shapes!
+// You need to use at least 3 different types of shape.
+// Remember, for the shapes to show up on the canvas, you'll need to CALL the function
+function createThirdScene() {
+makeCircle(150,50,15,"#6699CC",0.25)
+var y = Math.random()
+if(y < 0.67) {
+  makeCircle(170,60,15,"#8A2BE2",0.25) 
+}  else {
+    makeCircle(180,50,15,"#A2A2D0",0.25)
+}
+}
+    
+
+    
+
+    // FILL IN THIS FUNCTION!
+// This function is called whenever you press the "Go!" button.
+function createRandomScene() {
+    // Generate a random number between 0 and 1, and store it in a variable.
+var rando = Math.random()
+    // If the number is less than 0.33, call the function to create your first scene.
+ if(rando < 0.33) {
+    createFirstScene()
+}
+    // Else, if the number is less than 0.67, call the function to create your second scene.
+    else if(rando < 0.67) {
+        createSecondScene()
+}
+
+    // Else, call the function to create your third scene.
+  else {
+      createThirdScene()
+  }
+}
+
+
+
+// DO NOT EDIT ANYTHING BELOW THIS LINE!
+// These are the functions you should call to
+// easily create shapes in JavaScript. Feel free
+// to check them out if you're curious how they
+// work, but don't change them!
+// <3 Ms. Squires
+function makeCircle(cx, cy, r, fill, opacity) {
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", cx)
+  circle.setAttribute("cy", cy)
+  circle.setAttribute("r", r)
+  circle.setAttribute("fill", fill)
+  circle.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(circle)
+  return circle
+}
+
+function makeRect(x, y, width, height, fill, opacity) {
+  var rect = document.createElementNS(namespace, "rect")
+  rect.setAttribute("x", x)
+  rect.setAttribute("y", y)
+  rect.setAttribute("width", width)
+  rect.setAttribute("height", height)
+  rect.setAttribute("fill", fill)
+  rect.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(rect)
+  return rect
+}
+
+function makeEllipse(cx, cy, rx, ry, fill, opacity) {
+  var ellipse = document.createElementNS(namespace, "ellipse")
+  ellipse.setAttribute("cx", cx)
+  ellipse.setAttribute("cy", cy)
+  ellipse.setAttribute("rx", rx)
+  ellipse.setAttribute("ry", ry)
+  ellipse.setAttribute("fill", fill)
+  ellipse.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(ellipse)
+  return ellipse
+}
+
+function makeLine(x1, y1, x2, y2, stroke, strokeWidth, opacity) {
+  var line = document.createElementNS(namespace, "line")
+  line.setAttribute("x1", x1)
+  line.setAttribute("y1", y1)
+  line.setAttribute("x2", x2)
+  line.setAttribute("y2", y2)
+  line.setAttribute("stroke", stroke)
+  line.setAttribute("stroke-width", strokeWidth)
+  line.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(line)
+  return line
+}
+
+function makePolyline(points, stroke, strokeWidth, opacity) {
+  var polyline = document.createElementNS(namespace, "polyline")
+  polyline.setAttribute("points", points)
+  polyline.setAttribute("stroke", stroke)
+  polyline.setAttribute("stroke-width", strokeWidth)
+  polyline.setAttribute("opacity", opacity)
+  polyline.setAttribute("fill", "none")
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(polyline)
+  return polyline
+}
+
+function makePolygon(points, fill, opacity) {
+  var polygon = document.createElementNS(namespace, "polygon")
+  polygon.setAttribute("points", points)
+  polygon.setAttribute("opacity", opacity)
+  polygon.setAttribute("fill", fill)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(polygon)
+  return polygon
+}
+
+function makeText(message, x, y, fontSize, fontFamily, fill, opacity) {
+  var text = document.createElementNS(namespace, "text")
+  text.innerHTML = message
+  text.setAttribute("x", x)
+  text.setAttribute("y", y)
+  text.setAttribute("font-size", fontSize)
+  text.setAttribute("font-family", fontFamily)
+  text.setAttribute("fill", fill)
+  text.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(text)
+  return text
+}
+
+function makeImage(url, x, y, width, height, opacity) {
+  var image = document.createElementNS(namespace, "image")
+  image.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", url)
+  image.setAttribute("x", x)
+  image.setAttribute("y", y)
+  image.setAttribute("width", width)
+  image.setAttribute("height", height)
+  image.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(image) 
+    return image
+
+}
